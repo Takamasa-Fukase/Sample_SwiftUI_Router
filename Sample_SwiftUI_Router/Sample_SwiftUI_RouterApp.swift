16f77964425aa46350c064af7ab0d8355ddb0532
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-30 16:17:02 +0900

    Initial Commit

---
+++ Sample_SwiftUI_Router/Sample_SwiftUI_RouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_SwiftUI_RouterApp.swift
+//  Sample_SwiftUI_Router
+//
+//  Created by ウルトラ深瀬 on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Sample_SwiftUI_RouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
