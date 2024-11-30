f663fc97c43217fa5b11f3639ac42c36caadb4b1
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-30 16:36:34 +0900

    no message

---
+++ Sample_SwiftUI_Router/PostList/PostListView.swift
@@ -0,0 +1,52 @@
+//
+//  PostListView.swift
+//  Sample_SwiftUI_Router
+//
+//  Created by ウルトラ深瀬 on 30/11/24.
+//
+
+import SwiftUI
+
+struct PostListView: View {
+    let postList: [String] = [
+        "Post1",
+        "Post2",
+        "Post3"
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 32) {
+                ZStack(alignment: .center, content: {
+                    ScrollView {
+                        ForEach(postList, id: \.self) { post in
+                            Text(post)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 60)
+                                .background(Color(.secondarySystemBackground))
+                        }
+                    }
+                    
+                    VStack {
+                        Spacer()
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("+ Create Post")
+                                .font(.system(size: 20))
+                                .foregroundStyle(Color(.systemBackground))
+                        })
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 60)
+                        .background(Color(.label))
+                    }
+                })
+            }
+            .navigationTitle("PostList")
+        }
+    }
+}
+
+#Preview {
+    PostListView()
+}
